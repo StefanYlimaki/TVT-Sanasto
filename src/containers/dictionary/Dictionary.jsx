99ff33d5f6b586144450d862ae054d91b6664f18
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-12-12 14:58:19 +0200

    deleted useless code

--- src/containers/dictionary/Dictionary.jsx
+++ src/containers/dictionary/Dictionary.jsx
@@ -1,33 +1,11 @@
 import React, { useState } from "react";
 import { Button } from "@mui/material";
-import { styled as mstyled } from '@mui/material/styles';
 
 import "./dictionary.css";
 import WordList from "../../components/wordList/WordList";
 import SingleWord from "../../components/singleWord/SingleWord";
 import comp_basic from "../../assets/data/comp-basic.json";
 import networks_basic from "../../assets/data/networks-basic.json"
-
-import InputBase from '@mui/material/InputBase';
-
-const BootstrapInput = mstyled(InputBase)(({ theme }) => ({
-  'label + &': {
-    marginTop: theme.spacing(3),
-  },
-  '& .MuiInputBase-input': {
-    borderRadius: 10,
-    position: 'relative',
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #6F38C5',
-    fontSize: 20,
-    padding: '12px 0px 11px 10px',
-    fontFamily: [].join(','),
-    '&:focus': {
-      borderRadius: 10,
-      borderColor: '#6F38C5',
-    },
-  },
-}));
 
 const CompBasicCategory = () => {
   const [category, setGategory] = useState("comp_basic")
@@ -64,7 +42,6 @@ const CompBasicCategory = () => {
             <select
             value={category}
             onChange={(e)=>{setGategory(e.target.value); setWord(null); setSearch('')}}
-            input={<BootstrapInput />}
             >
               <option value={"comp_basic"}>Tietotekniikan Perustermistöä</option>
               <option value={"networks_basic"}>Internet ja Tietoverkot</option>
