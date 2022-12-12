05f8e2c2a79354ef28b3d3a7245ef9c113185863
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-12-12 11:40:53 +0200

    Made homescreen better 4 devices w/ smaller screen

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -6,13 +6,12 @@ const Home = () => {
     <div className='tvt__home'>
       <div className='tvt__home-description'>
         <p>Tämän sovelluksen tarkoituksena on selkeyttää tietotekniikan alan opiskelussa käytettyjä termejä ja lyhenteitä.</p>
-        <p>Sovelluksessa on tällä hetkellä kaksi kategoriaa: <strong>Tietotekniikan Perustermistöä</strong> sekä <strong>Internet ja Tietoverkot</strong></p>
-        <br />
+        <p>Sovelluksessa on tällä hetkellä kaksi kategoriaa: 
+        <strong>Tietotekniikan Perustermistöä</strong> sekä <strong>Internet ja Tietoverkot</strong></p>
         <br />
         <br />
         <p>Sovelluksen verkkoversion on kehittänyt <strong><a href='https://www.linkedin.com/in/stefan-ylim%C3%A4ki/'>Stefan Ylimäki</a></strong></p>
       </div>
-      
     </div>
   )
 }
