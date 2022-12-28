13d596a48879242b2189a5f2691e4d87c98256a3
Stefan Ylimäki <71447366+StefanYlimaki@users.noreply.github.com>
2022-12-28 15:18:42 +0200

    Match word and translation game (#20)
    * Continued to develop the new game

--- tvt-sanasto-frontend/src/containers/games/matchWordAndTranslation/gameSettings/GameSettings.jsx
+++ tvt-sanasto-frontend/src/containers/games/matchWordAndTranslation/gameSettings/GameSettings.jsx
@@ -1,13 +1,126 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 import './gameSettings.css'
+import { Slider } from '@mui/material'
 
 const GameSettings = ({
-  setGameRunning
+  setCategory,
+  gameLength,
+  setGameLength,
+  setErrorMessage,
+  setGameRunning,
+  amountOfOptions,
+  setAmountOfOptions
 }) => {
-  return (
-    <div>
-      <p>GameSettings</p>
-      <button onClick={ () => setGameRunning(true) }>start game</button>
+
+  const [selectedCategory, setSelectedCategory] = useState(null)
+
+  const marksRounds = [
+    {
+      value: 4,
+      label: '4',
+    },
+    {
+      value: 10,
+      label: '10',
+    },
+    {
+      value: 20,
+      label: '20',
+    },
+  ]
+
+  const marksOptions = [
+    {
+      value: 2,
+      label: '2',
+    },
+    {
+      value: 4,
+      label: '4',
+    },
+    {
+      value: 6,
+      label: '6',
+    },
+    {
+      value: 8,
+      label: '8',
+    },
+  ]
+
+  const handleRoundsSliderChange = (event, newValue) => {
+    setGameLength(newValue)
+  }
+
+  const handleOptionsSliderChange = (event) => {
+    setAmountOfOptions(event.target.value)
+  }
+
+  const handleStartGameClick = () => {
+    if (selectedCategory !== null) {
+      setCategory(selectedCategory)
+      setGameRunning('true')
+    } else {
+      setErrorMessage('Valitse kategoria')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 2000)
+    }
+  }
+
+  return(
+    <div className="game__settings">
+      <div className="game__setting-gamelength">
+        <p>Valitse kierrosten lukumäärä:</p>
+        <div className="game__setting-gamelength_slider">
+          <Slider
+            defaultValue={10}
+            min={4}
+            max={20}
+            valueLabelDisplay="auto"
+            marks={marksRounds}
+            value={gameLength}
+            onChange={handleRoundsSliderChange}
+          />
+        </div>
+      </div>
+      <div className="game__setting-gamelength">
+        <p>Valitse vaihtoehtojen lukumäärä:</p>
+        <div className="game__setting-gamelength_slider">
+          <Slider
+            defaultValue={4}
+            min={2}
+            max={8}
+            valueLabelDisplay="auto"
+            marks={marksOptions}
+            value={amountOfOptions}
+            onChange={handleOptionsSliderChange}
+          />
+        </div>
+      </div>
+      <div className="game__settings-category">
+        <p>Valitse kategoria:</p>
+        <div className="game__settings-category_buttons">
+          <button
+            onClick={() => {
+              setSelectedCategory('basic-comp')
+            }}
+          >
+            Tietotekniikan Perustermistöä
+          </button>
+          <button
+            onClick={() => {
+              setSelectedCategory('internet-basic')
+            }}
+          >
+            Internet ja Tietoverkot
+          </button>
+        </div>
+      </div>
+      <div className="game__settings-start_game">
+        <button onClick={handleStartGameClick}>Aloita peli</button>
+      </div>
     </div>
   )
 }
--- tvt-sanasto-frontend/src/containers/games/matchWordAndTranslation/gameplay/Gameplay.jsx
+++ tvt-sanasto-frontend/src/containers/games/matchWordAndTranslation/gameplay/Gameplay.jsx
@@ -1,27 +1,190 @@
 import { Button } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Scoreboard from '../scoreboard/Scoreboard'
+import shuffleArray from '../../../../utils/shuffleArray'
 import './gameplay.css'
 
 const Gameplay = ({
-  setGameRunning
+  category,
+  gameLenght,
+  setGameRunning,
+  amountOfOptions
 }) => {
+  let words = []
+  if (category === 'basic-comp') {
+    words = JSON.parse(localStorage.getItem('basic-comp'))
+  } else {
+    words = JSON.parse(localStorage.getItem('internet-basic'))
+  }
+
+  const [points, setPoints] = useState(0)
+  const [round, setRound] = useState(0)
+
+  const [usedWordIndexes, setUsedWordIndexes] = useState([])
+  const [usedRandomWordsIndexes, setUsedRandomWordsIndexes] = useState([])
 
   const [gameHasEnded, setGameHasEnded] = useState(false)
+  const [gameIsLoading, setGameIsLoading] = useState(true)
+
+  const [randomWord, setRandomWord] = useState()
+  const [optionWords, setOptionWords] = useState([])
+  const [correctAnswer, setCorrectAnswer] = useState({})
+
+  const [questions, setQuestions] = useState([])
+  const [answers, setAnswers] = useState([])
+  const [options, setOptions] = useState([])
+
+  const wordsToUse = []
+
+
+  useEffect(() => {
+    startGame()
+  }, [])
+
+  // function for getting a random word, which's index is not in usedWordIndexes
+  const getRandomWord = (isToBeGuessed) => {
+    let index = Math.floor(Math.random() * words.length) // Get random integer between 0 and lenght of words array
+    if (isToBeGuessed === true) { // If word is going to be the word to guess, then check that it is not been guessed earlier in the game
+      for (let i = 0; usedRandomWordsIndexes.includes(index); i++) {
+        index = Math.floor(Math.random() * words.length)
+      }
+      setCorrectAnswer(words[index])
+      setUsedRandomWordsIndexes(usedRandomWordsIndexes.concat(index))
+    } else {
+      for (let i = 0; usedWordIndexes.includes(index); i++) {
+        index = Math.floor(Math.random() * words.length)
+      }
+    }
+    setUsedWordIndexes(usedWordIndexes.push(index))
+    return words[index]
+  }
+
+  const startGame = () => {
+    setGameIsLoading(false)
+    nextRound(false)
+  }
+
+  const nextRound = (correctAnswer) => {
+    if (round === gameLenght) {
+      if(correctAnswer) endGame(true)
+      endGame(false)
+    }
+
+    if (correctAnswer) {
+      setPoints(points + 1)
+    }
+
+    setRound(round + 1)
+
+    const newRandomWord = getRandomWord(true)
+    setQuestions(questions.concat(newRandomWord))
+    setRandomWord(newRandomWord)
+
+    // get definitions to use for options in the round
+    for(let i = 1; i < amountOfOptions; i++){
+      wordsToUse.push((getRandomWord(false)))
+    }
+    wordsToUse.push((newRandomWord))
+
+    // saving options for later use
+    setOptions(options.concat(wordsToUse))
+
+    setUsedWordIndexes([])
+    setOptionWords(shuffleArray(wordsToUse))
+  }
+
+  const handleClickOnOption = (word) => {
+    setAnswers(answers.concat(word))
+    // if selected option's (word selected) id matches with that of correct answer's
+    if (word.id === correctAnswer.id) {
+      nextRound(true)
+    } else {
+      nextRound(false)
+    }
+  }
+
+  const generateGameRaport = () => {
+    const raport = {
+      points: points,
+      rounds: round - 1,
+      category_id: category,
+      questions: questions,
+      answers: answers,
+      options: options
+    }
+    return raport
+  }
+
+  const endGame = () => {
+    setGameHasEnded(true)
+  }
 
   return (
     <div>
-      { gameHasEnded
-        ?
-        <Scoreboard
-          setGameRunning = { setGameRunning }
-        />
-        :
-        <div>
-          <p>Gameplay</p>
-          <Button onClick={ () => setGameHasEnded(true) } >Lopeta Peli</Button>
+      { gameIsLoading
+        ? ( <div>game loading...</div> )
+        : ( <div className='game__gameplay'>
+          { gameHasEnded
+            ? (<div><Scoreboard
+              setGameRunning = { setGameRunning }
+              points = { points }
+              rounds = { gameLenght }
+              raport = { generateGameRaport() }
+            /></div>)
+            : (<div className="game_gameplay-main">
+              <div className="game__gameplay-stats_and-text_length">
+                <div className="game__gameplay-stats_and-text_length-stats">
+                  <div>
+                    Kierros&nbsp;
+                    {round}
+                    /
+                    {gameLenght}
+                  </div>
+                  <div>
+                    Pisteet&nbsp;
+                    {points}
+                    /
+                    {round - 1}
+                  </div>
+                </div>
+              </div>
+
+              <div className="game__gameplay-advice">
+                <div className="game__gameplay-advice_text">
+                  Mikä seuraavista käännöksistä sopii sanalle:
+                </div>
+                {randomWord !== null ? (
+                  <div className="game__gameplay-advice_word">
+                    {randomWord.finnish}
+                    {' '}
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+              <div className="game__gameplay-options">
+                {optionWords !== null ? (
+                  <div className="game__gameplay-options_single">
+                    {optionWords.map((w) => (
+                      <div
+                        key={w.english}
+                        className="game__gameplay-options_single-option"
+                        onClick={() => handleClickOnOption(w)}
+                      >
+                        <p>{w.english}</p>
+
+                      </div>
+                    ))}
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+              <Button variant="contained" onClick={() => endGame()}>Lopeta peli</Button>
+            </div>)
+          }
         </div>
-      }
+        )}
     </div>
   )
 }
