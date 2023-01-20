8d9bdf59a63be3ba64adea2119a43e2c3d6fdbf0
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2023-01-20 10:36:48 +0200

    trying to fix probelms

--- tvt-sanasto-api/controllers/data.js
+++ tvt-sanasto-api/controllers/data.js
@@ -4,24 +4,28 @@ const fetchJSONData = require("../utils/helpers");
 
 router.get("/:id", async (req, res) => {
   if (req.params.id === "basic-comp") {
-    Dictionary.find({ name: "comp-basic" }).then((dictionary) => {
-      res.json(dictionary);
-    });
+    Dictionary.find({ name: "comp-basic" })
+      .then((dictionary) => {
+        res.json(dictionary);
+      })
+      .catch((error) => console.log(error));
   } else if (req.params.id === "internet-basic") {
-    Dictionary.find({ name: "networks-basic" }).then((dictionary) => {
-      res.json(dictionary);
-    });
+    Dictionary.find({ name: "networks-basic" })
+      .then((dictionary) => {
+        res.json(dictionary);
+      })
+      .catch((error) => console.log(error));
   } else {
     res.send("malformatted id value in url");
   }
 });
 
 router.get("/", async (req, res) => {
-  await fetchJSONData(
-    "https://gitlab.com/sanasto/index/-/raw/main/index.json"
-  ).then((response) => {
-    res.send(response);
-  });
+  await fetchJSONData("https://gitlab.com/sanasto/index/-/raw/main/index.json")
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((error) => console.log(error));
 });
 
 module.exports = router;
